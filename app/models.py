dcbf18f3c06e97a8951ad694ff3211c602e7caf4
michael-muga <mugamichaelshawn@gmail.com>
2022-03-12 20:18:30 +0300

    password varification functionality added

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from enum import unique
-from sqlalchemy import PrimaryKeyConstraint
+from sqlalchemy.sql import func
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.Strring(250), unique = True)
+    email = db.Column(db.String(250), unique = True)
     password = db.Column(db.String(250))
     username = db.Column(db.String(250))
+
+    pitches = db.relationship('Pitch', backref='owner')
+    comments = db.relationship('Comment', backref='owner')
+
 
     def __repr__(self):
         return f"User('{self.firstname}')"
@@ -24,6 +30,18 @@ class Pitch(db.Model):
     category = db.Column(db.String(255), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     pass_secure = db.Column(db.String(255))
+
+    @property 
+    def password(self):
+        raise AttributeError("you cannot read the password attribute")
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+        
 
     def __repr__(self):
         return f'Pitch {self.description}'
