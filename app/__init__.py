452c8afc1719472d665aaa05f7529a3207368f4b
michael-muga <mugamichaelshawn@gmail.com>
2022-03-12 20:17:41 +0300

    added auth blueprint and login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+
+login_manager.session_protection = 'strong'
 
 
 def create_app(config_name):
@@ -17,9 +21,13 @@ def create_app(config_name):
     #initialising flask extension
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     #registering blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix = '/')
 
     return app
