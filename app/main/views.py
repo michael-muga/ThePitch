929e631f0516f61ece2302459592f775122b4cfc
michael-muga <mugamichaelshawn@gmail.com>
2022-03-13 11:38:45 +0300

    added routes for the user bio data

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from crypt import methods
 from flask import render_template,request,redirect,url_for
 from . import main
-from . forms import LoginForm, RegisterForm, PitchForm, CommentForm
+from . forms import LoginForm, RegisterForm, PitchForm, CommentForm, UpdateProfile
 from ..models import User, Pitch, Comment
 from flask_login import login_required, current_user, logout_user
-from .. import db
+from .. import db, photos
 
 
 @main.route('/')
@@ -34,14 +34,46 @@ def pitch():
         db.session.commit()
     return render_template('pitch.html', form=form)
 
-@main.route('/profile')
+@main.route('/user/<uname>')
 @login_required
-def profile():
+def profile(uname):
 
     pitch = Pitch.query.filter_by(id=current_user.id).all()
+    user = User.query.filter_by(username=uname).first()
 
-    return render_template('profile.html', name=current_user.username, email=current_user.email, password=current_user.password, pitch=pitch)
+    return render_template('profile/profile.html', name=current_user.username, email=current_user.email, password=current_user.password, user=user, pitch=pitch)
 
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        return redirect(url_for('.error'))
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
+
+
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
 
 @main.route('/logout')
 @login_required
