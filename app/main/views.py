5d3dbe0b224eb255797457d2207f004f852c7b7c
michael-muga <mugamichaelshawn@gmail.com>
2022-03-13 10:23:14 +0300

    added new routes pitch profile and logout

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
+from crypt import methods
 from flask import render_template,request,redirect,url_for
 from . import main
 from . forms import LoginForm, RegisterForm, PitchForm, CommentForm
 from ..models import User, Pitch, Comment
+from flask_login import login_required, current_user, logout_user
+from .. import db
 
 
 @main.route('/')
@@ -14,3 +17,34 @@ def index():
 def error():
 
     return render_template('error.html')
+
+@main.route('/pitch', methods = ['GET','POST'])
+@login_required
+def pitch():
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        pitch = Pitch(owner_id=current_user.id, title=form.title.data,
+                      category=form.category.data, description=form.description.data)
+        form.title.data = ''
+        form.category.data = ''
+        form.description.data = ''
+
+        db.session.add(pitch)
+        db.session.commit()
+    return render_template('pitch.html', form=form)
+
+@main.route('/profile')
+@login_required
+def profile():
+
+    pitch = Pitch.query.filter_by(id=current_user.id).all()
+
+    return render_template('profile.html', name=current_user.username, email=current_user.email, password=current_user.password, pitch=pitch)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
